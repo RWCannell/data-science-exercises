ca26e13d96b1116da7258fb0aeb8db56ca52b09c
RWCannell <regancannell@gmail.com>
2024-08-09 15:21:35 +0200

    Converted date to date_time format

--- scripts/get_south_african_exchange_rate_data_against_euro.py
+++ scripts/get_south_african_exchange_rate_data_against_euro.py
@@ -11,9 +11,13 @@ desired_columns = [
     'value'
 ]
 
+
 south_african_exchange_rates_df = full_south_african_exchange_rates_df[desired_columns]
 
 south_african_exchange_rates_df.reset_index(drop=True, inplace=True)
+
+south_african_exchange_rates_df['date'] = pd.to_datetime(south_african_exchange_rates_df['date'], dayfirst=True)
+south_african_exchange_rates_df['value']= pd.to_numeric(south_african_exchange_rates_df['value'])
 
 csv_name = "south_african_exchange_rate_against_euro.csv"
 south_african_exchange_rates_df.to_csv(f"csv\{csv_name}")
