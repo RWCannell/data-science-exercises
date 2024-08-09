d3327d356bd3824c59a5a8039e2f792c7d2471c2
RWCannell <regancannell@gmail.com>
2024-08-09 15:09:14 +0200

    Cleaned South African exchange rate data

--- scripts/get_south_african_exchange_rate_data_against_euro.py
+++ scripts/get_south_african_exchange_rate_data_against_euro.py
@@ -3,7 +3,17 @@ import pandas as pd
 full_exchange_rates_df = pd.read_csv("csv/exchange_rates_against_euro.csv")
 
 selected_currency = 'ZAR'
-south_african_exchange_rates_df = full_exchange_rates_df[full_exchange_rates_df['currency'].str.contains(selected_currency)]
+full_south_african_exchange_rates_df = full_exchange_rates_df[full_exchange_rates_df['currency'].str.contains(selected_currency)]
+
+desired_columns = [
+    'date',
+    'currency',
+    'value'
+]
+
+south_african_exchange_rates_df = full_south_african_exchange_rates_df[desired_columns]
+
+south_african_exchange_rates_df.reset_index(drop=True, inplace=True)
 
 csv_name = "south_african_exchange_rate_against_euro.csv"
 south_african_exchange_rates_df.to_csv(f"csv\{csv_name}")
